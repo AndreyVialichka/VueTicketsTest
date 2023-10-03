1069ee6a45fb43ca67e14714d48b2184124e4ae9
AndreyVialichka <forruexpress12@gmail.com>
2023-10-03 05:45:15 +0300

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 import {resolve} from "path"
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/VueTicketsTest/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   devtools: { enabled: true },
   alias: {
     "@": resolve(__dirname, "/")
